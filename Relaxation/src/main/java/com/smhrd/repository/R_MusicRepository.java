c8da9bcf04cd6f5466bf71c97e77c656a99e4a8a
USER <USER@DESKTOP-TM5U3FN>
2023-11-02 15:20:22 +0900

    푸시푸시뻬이베나를안아조

--- Relaxation/src/main/java/com/smhrd/repository/R_MusicRepository.java
+++ Relaxation/src/main/java/com/smhrd/repository/R_MusicRepository.java
@@ -11,12 +11,30 @@ import com.smhrd.entity.R_Music;
 @Repository
 public interface R_MusicRepository extends JpaRepository<R_Music, Long>{
 
-	// 메인페이지 Top10가수 띄워주기위한 쿼리문, 배열에 담기
+	// 메인페이지 Top7가수 띄워주기위한 쿼리, 리스트에담기
 	@Query(value = "SELECT rmu_singer " +
             "FROM r_music " +
             "GROUP BY rmu_singer " +
             "ORDER BY COUNT(*) DESC " +
-            "LIMIT 10", nativeQuery = true)
-	List<String> findTop10();
+            "LIMIT 7", nativeQuery = true)
+	List<String> findTop7();
+	
+	// 랜덤으로 7개 음악가지고 오는 쿼리, 리스트에담기
+	@Query(value = "SELECT * " +
+	        "FROM r_music " +
+	        "WHERE rmu_seq BETWEEN 1734 AND 5792 " +
+	        "ORDER BY RAND() " +
+	        "LIMIT 7", nativeQuery = true)
+	List<R_Music> findRandom7();
+	
+	// 특정 한 가수 노래 7개 랜덤으로 가지고오는 쿼리, 리스트에담기(가수 이름만 바꾸면 됨)
+	@Query(value = "SELECT * " +
+	        "FROM r_music " +
+	        "WHERE rmu_singer = 'iu' " +
+	        "ORDER BY RAND() " +
+	        "LIMIT 7", nativeQuery = true)
+	List<R_Music> findRandom7BySinger();
+	
+	
 	
 }
